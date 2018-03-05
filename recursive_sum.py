d374cdeca28bb97ffae13b9fd85053e7f7f034c1
AjayShelar <ajaysanjayshelar@hotmail.com>
2018-03-05 12:39:25 +0530

    from geeksforgeeks

--- recursive_sum.py
+++ recursive_sum.py
@@ -1,3 +1,24 @@
+
+
+"""https://www.geeksforgeeks.org/recursive-sum-digits-number-formed-repeated-appends/"""
+
+"""Given two positive number N and X. The task is to find the sum of digits of a number formed by N repeating X number of times until sum become single digit.
+
+Examples:
+
+Input : N = 24, X = 3
+Output : 9
+Number formed after repeating 24 three time = 242424
+Sum = 2 + 4 + 2 + 4 + 2 + 4
+    = 18
+Sum is not the single digit, so finding 
+the sum of digits of 18,
+1 + 8 = 9
+
+Input : N = 4, X = 4
+Output : 7
+"""
+
 def digSum(n):
  
     if (n == 0):
